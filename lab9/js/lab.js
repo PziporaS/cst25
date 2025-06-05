737c2a00799f4ff178e269368db84c88bcd99e8a
PziporaS <pshepher@ucsc.edu>
2025-06-05 16:38:12 -0700

    Lab 9
    Task X

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -7,29 +7,11 @@
    Date: 2025
 */
 
-// add button to challenge section
-$("#challenge").append("<button id='button-challenge'>Make Special</button>");
-
-// add button to problems section
-$("#problems").append("<button id='button-problems'>Make Special</button>");
-
-// add button to results section
-$("#results").append("<button id='button-results'>Make Special</button>");
+// add button to challenge, problem and results sections
+$(".special-secions").append("<button class='button-special'>Make Special</button>");
 
 // add a click listener to the challenge button
-$("#button-challenge").click(function(){
+$(".special-secions button").click(function(){
     // now add (or subtract) the "special" class to the section
-    $("#challenge").toggleClass("special");
-});
-
-// add a click listener to the problems button
-$("#button-problems").click(function(){
-    // now add (or subtract) the "special" class to the section
-    $("#problems").toggleClass("special");
-});
-
-// add a click listener to the results button
-$("#button-results").click(function(){
-    // now add (or subtract) the "special" class to the section
-    $("#results").toggleClass("special");
+    $(this).parent().toggleClass("special");
 });
