ef3ff2cdb17642dba14ab10ab5c67b78eb039642
PziporaS <pshepher@ucsc.edu>
2025-06-08 11:04:03 -0700

    Lab 8
    Task X

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -1,8 +1,6 @@
-/*
-* Author: Pzipora Shepherd
-* Created: 05/09/25
-* License: Public Domain
-*/
+// index.js - experiment with anonymous functions and callbacks.
+// Author: Pzipora Shepherd
+// Date: 05/09/25
 
 // Functions
 function isEven(x){
@@ -16,12 +14,21 @@ console.log("Is 2 even? ", isEven(2));
 array = [100, 81, 4, 16, 42, 144, 10000]
 console.log("My array", array);
 
-var result = array.map(isEven);
+var isEvenResults = array.map(isEven);
 // should return [true, false, true, true, true, true, true]
-console.log("Test of evenness of array ", result);
+console.log("Test of evenness of array ", isEvenResults);
 
-var result = array.map(function(x){
+var squareRootResults = array.map(function(x){
     return x ** 0.5;
 })
 // should return [10, 9, 2, 4, 6.48074069840786, 12, 100]
-console.log("Squareroot of array:", result);
+console.log("Squareroot of array:", squareRootResults);
+
+// Your map results data
+var mapResults = "<p><s>Go to the console to see the output.</s> The output will appear here!</p>"
+
+// Use jQuery to select the element by its ID and set the HTML content
+$("#output").html(mapResults);
+$("#output").append("Original array: " + JSON.stringify(array) + "<br>");
+$("#output").append("Evenness of array: " + JSON.stringify(isEvenResults) + "<br>");
+$("#output").append("Squareroots of array: " + JSON.stringify(squareRootResults));
