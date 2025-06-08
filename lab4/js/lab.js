6d57a8bcb8581b8856254c2f7ef294bb12020d72
PziporaS <pshepher@ucsc.edu>
2025-06-07 18:34:07 -0700

    Lab 4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,15 +1,21 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Pseudocode a simple computer game: Rock, Paper, Scissors
+// Author: Pzipora Shepherd
+// Date: June 2025
 
 // Constants
 
 // Functions
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
+// this function creates the computer game Rock, Paper, Scissors
 function myFunction(param1, param2) {
-  // some code here
-  // return results;
+  // Choose one of the three objects: Rock, Paper, Scissors
+  // Hide chosen object from opponent
+  // Countdown from 3 to 1
+  // Present the chosen objects at the same time after countdown
+  // Win or lose
+  // Start over
+  // return results; One person wins
 }
 
 function main() {
