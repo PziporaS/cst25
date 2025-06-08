e11274159af0a057bbe1a1fd779d88240be71bb3
PziporaS <pshepher@ucsc.edu>
2025-06-08 10:15:59 -0700

    Lab 8

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -1,21 +1,27 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
-
-// Constants
+/*
+* Author: Pzipora Shepherd
+* Created: 05/09/25
+* License: Public Domain
+*/
 
 // Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+function isEven(x){
+    return (x % 2 == 0);
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+// test function
+console.log("Is 1 even? ", isEven(1));
+console.log("Is 2 even? ", isEven(2));
 
-// let's get this party started
-main();
+array = [100, 81, 4, 16, 42, 144, 10000]
+console.log("My array", array);
+
+var result = array.map(isEven);
+// should return [true, false, true, true, true, true, true]
+console.log("Test of evenness of array ", result);
+
+var result = array.map(function(x){
+    return x ** 0.5;
+})
+// should return [10, 9, 2, 4, 6.48074069840786, 12, 100]
+console.log("Squareroot of array:", result);
