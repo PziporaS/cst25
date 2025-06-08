975852958db44384d618742fb2e68c12f07a1447
PziporaS <pshepher@ucsc.edu>
2025-06-08 00:05:04 -0700

    Lab 5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,19 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - experiment with basic JavaScript data types and variables.
+// Author: Pzipora Shepherd
+// Date: 2025
 
-// Constants
+// Define Variables
+make = "Volkswagen";
+model = "Passat";
+color = "Black";
+year = "2016";
 
-// Functions
+// calculate
+age = 2025 - year;
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
